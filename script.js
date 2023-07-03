239f7cdab19c3ea1bebe133a9737fab7a04c4668
Leonardo Chinelato <66490205+leochinelato@users.noreply.github.com>
2023-07-03 11:26:31 -0300

    Upload do site de gerador de senhas

---
+++ script.js
@@ -0,0 +1,20 @@
+function gerSenha() {
+    const maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const minusculas = 'abcdefghijklmnopqrstuvwxyz';
+    const especiais = '!@#$%^&*()';
+    const numeros = '0123456789';
+
+    let password = '';
+
+    password += maiusculas.charAt(Math.floor(Math.random() * maiusculas.length));
+    password += minusculas.charAt(Math.floor(Math.random() * minusculas.length));
+    password += especiais.charAt(Math.floor(Math.random() * especiais.length));
+    password += numeros.charAt(Math.floor(Math.random() * numeros.length));
+
+    for (let i = 0; i < 4; i++) {
+        const charSet = maiusculas + minusculas + especiais + numeros;
+        password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    }
+
+    document.getElementById('password').value = password;
+}
